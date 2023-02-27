6c1eec633d0253f83c6e8f2208d071ab6dc80bae
alenmonti <amonti@fi.uba.ar>
2023-02-27 20:40:18 -0300

    ds

--- prueba dropdow/script.js
+++
@@ -1,8 +0,0 @@
-const dropdowns = document.querySelectorAll('.dropdown');
-
-dropdowns.forEach((dropdown) => {
-  dropdown.addEventListener('click', (event) => {
-    event.preventDefault();
-    dropdown.classList.toggle('active');
-  });
-});
